38b4a220689588ddc6f5840d2d7acd16ab8133d9
macklo001 <adres_macklo001@utp.edu.pl>
2021-03-17 18:11:46 +0100

    java V01

---
+++ out/production/my_first_project/src/main/java/pl/edu/utp/Application.java
@@ -0,0 +1,9 @@
+package pl.edu.utp;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world from UTP Java course!");
+    }
+}
+
--- src/main/java/pl/edu/utp/Application.java
+++ src/main/java/pl/edu/utp/Application.java
@@ -1,4 +1,13 @@
-package pl.edu.utp;
+// komentarz jednolinijkowy
+// deklaracja paczki
+ package pl.edu.utp;
+
+//deklaracje importów wszystkie klasy w tej samej paczce siebie widzą i nie musimy używać deklaracji Import
 
 public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world from UTP Java course!");
+    }
 }
+
